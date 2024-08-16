25f782e9c9ccf4d3fafe5a074463637e31f07531
Bhabani Sahu <info@167labs.com>
2024-08-15 22:06:08 -0700

    First commit

---
+++ src/main/java/com/example/demo/PassChangeApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PassChangeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PassChangeApplication.class, args);
+	}
+
+}
